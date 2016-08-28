d056d843b7eb78acefcb299a7ee91d48aa9c7346
Brandon Davis <bdavis@redspin.net>
2016-08-28 17:56:14 -0400

    Ready to roll as soon as venue confirmed. (#36)
    * UPDATE- git workflow
    
    * Update README.md
    
    * re-add mobile fix #10000 and reposition socials
    
    * Social updates (#35)
    
    * re-add mobile fix #10000 and reposition socials
    
    * update social, remove ram on mobile bug, add critical info
    
    * add favicon
    
    * add mlh banner
    
    * convert to terrible banner quality :)
    
    * Last year's sponsors'
    
    * Gitlink and mailchimp color/size
    
    * idk what I'm doing
    
    * JS is terrible
    
    * Accidentally deleted the 'row' class from about
    
    * finally got it working
    
    * Travel page finally looks decent
    
    * external pages shells
    
    * faq re-imagined (#38)
    
    * The navbar commit - add travel and resources'
    
    * precompile markdown
    
    * navbar woes
    
    * registration is ok
    
    * Create LICENSE
    
    * Use easier markdown imports and resize MLH logo
    
    * Small changes like linking
    
    * Reduce size of MLH badge
    
    * Typos and grammar fixes
    
    * Other minor changes with spelling
    
    * update header images
    
    * Update travel page (#40)
    
    * More specific/updated wording
    * Less committal buses
    * Recommended arrival time
    
    * grammar fixes

--- js/main.js
+++ js/main.js
@@ -52,9 +52,11 @@ function mobileinit() {
         $(".apply").css("position", "relative");
         $(".parallax").css("display", "none");
         $(".mobile-no-parallax").css("display", "block");
-        $(".navi").addClass("relative");
-        $("#navbar").css("top", "460px")
-        $('#navbar').css("bottom", "auto")
+        $("#navbar").removeClass("relative");
+        $("#navbar").css('top', '1px');
+        $('.parallax').remove();
+        $('parallax-mirror').remove();
+        $('#navbar').css("bottom", "auto");
         $('.anchor').css("top", "0");
     } else {
         //setup for desktop browsers
@@ -68,17 +70,12 @@ function mobileinit() {
 function faqinit(){
     $(".faq").click(function(){
         if($(this).hasClass("toggled")) {
-            $(this).children(".faq-body").slideUp();
-            $(this).animate({"height": "100%"}).removeClass("toggled");
+            $(this).children(".faq-body").slideUp(200);
+            $(this).animate({"height": "100%"}, 200).removeClass("toggled");
         } else {
-            $(this).animate({"height": "250px"}).addClass("toggled");
-            $(this).children(".faq-body").slideDown();
+            $(this).animate({"height": "inherit"}, 200).addClass("toggled");
+            $(this).children(".faq-body").slideDown(200); 
         }
-        /*$(this).children(".faq-body").slideToggle("slow", function(){
-          if($(this).hasClass("toggled")){
-              $(this).animate({"height" : "330px"});
-          }  
-        });*/
     });
 }
 function navbarinit(){
@@ -117,4 +114,18 @@ function buttonCheckScroll(){
     } else {
         button.css("display", "inline-block");
     } 
+}
+function load_markdown(divstring, mdpath){
+    $(window).load(function(){
+        var elem = document.getElementById(divstring);
+        console.log(elem);
+        var callback = function(data){
+            var converter = new showdown.Converter(),
+            text      = data;
+            html      = converter.makeHtml(text);
+            console.log(html);
+            elem.innerHTML = html;  
+        }
+        $.get(mdpath, callback);
+    });
 }
